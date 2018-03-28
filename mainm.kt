e72540e2819895fdefb8a7f5a7a31ca1043af352
CodeFarmsJacob <36199310+CodeFarmsJacob@users.noreply.github.com>
2018-03-28 09:17:41 -0400

    Update mainm.kt

--- mainm.kt
+++ mainm.kt
@@ -1,42 +1,56 @@
+import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.FlowLayout
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.io.File
+import java.io.FileWriter
 import javax.swing.*
+import kotlin.math.absoluteValue
 
+var fileDebounce : Boolean = false
 var frame = JFrame("Kotlin IDE")
 var fileSelect = JFileChooser()
 
 var TextTextArea = JTextArea()
+
 var fileButton = JButton("Open File").apply { addActionListener(FileClick()) }
 var compileButton = JButton("Compile").apply { addActionListener(CompileClick()) }
 var saveButton = JButton("Save").apply {
 
     addActionListener(SaveClick())
 }
+//var compileKbutton = JButton("Compile in Kotlin ONLY").apply {addActionListener(compileKClick())}
+var scroll = JScrollPane(TextTextArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
+private class InsertMain : AbstractAction()  {
 
+    override fun actionPerformed(e: ActionEvent) {
+        TextTextArea.append("fun main(args: Array<String>)")
+    }
+}
 
 fun gui() {
 
     frame.apply {
 
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        setSize(800, 500)
+        setSize(1000, 1000)
         layout = FlowLayout()
 
 
     }
-
+    TextTextArea.preferredSize = Dimension( 500, 800)
     saveButton.preferredSize = Dimension(90, 20)
     compileButton.preferredSize = Dimension(90, 20)
     fileButton.preferredSize = Dimension(90, 20)
-    TextTextArea.preferredSize = Dimension(600, 400)
+
     frame.add(fileButton)
     frame.add(compileButton)
     frame.add(saveButton)
-    frame.add(TextTextArea)
-
+    frame.add(scroll)
+    TextTextArea.lineWrap = true
+    TextTextArea.inputMap.put(KeyStroke.getKeyStroke(" shift F1"), "doNothing")
+    TextTextArea.actionMap.put("doNothing", InsertMain())
     frame.apply { isVisible = true }
 }
 
@@ -46,8 +60,22 @@ private class SaveClick : ActionListener {
     override fun actionPerformed(e: ActionEvent) {
 
         println("Save Initialized")
+        if(fileDebounce) {
+           var leFile = fileSelect.selectedFile
+            leFile.bufferedWriter().use { out->
+                out.write(TextTextArea.text)
+            }
 
+            var writer = FileWriter(leFile, false)
+            TextTextArea.write(writer)
+        }
+        else {
+            var newFrame = JFrame("Save File")
+            var fie = fileSelect.showSaveDialog(newFrame)
+            newFrame.isVisible = true
+            println("InsertTestPathToTheFileSelect")
 
+        }
     }
 }
 
@@ -70,28 +98,31 @@ private class FileClick : ActionListener {
         newFrame.add(fileSelect)
         newFrame.isVisible = true
 
-    if(fileResult == JFileChooser.APPROVE_OPTION) {
-
-        var FileToOpen = fileSelect.selectedFile
-        println("$FileToOpen Selected")
-        newFrame.isVisible = false
-        var FileText = File(FileToOpen).read
-
-        TextTextArea.text = FileText
-
-
-        if(fileResult == JFileChooser.CANCEL_OPTION) {
-
+        if (fileResult == JFileChooser.APPROVE_OPTION) {
+            var FileToOpen = fileSelect.selectedFile
+            println("$FileToOpen Selected")
             newFrame.isVisible = false
+
+            if(fileSelect.selectedFile.exists()) {
+                var FileText = FileToOpen.readText()
+                scroll.layout =  ScrollPaneLayout()
+                TextTextArea.text = FileText
+                frame.isVisible = false
+                frame.isVisible = true
+                fileDebounce = true
+            }
+
+            if (fileResult == JFileChooser.CANCEL_OPTION) {
+
+                newFrame.isVisible = false
+            }
+
+
         }
-
-
-
     }
 }
 
 
-
 fun main(args: Array<String>) {
     gui()
-}}
+}
